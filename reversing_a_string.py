8ff4395e3b9b5d05e754fb6909c7fad423280917
paulgioeli1 <paulgioeli@gmail.com>
2026-01-05 20:17:34 -0800

    Reversing a string
    I don't know that I ever came across this before, but it's super cool!

---
+++ reversing_a_string.py
@@ -0,0 +1,15 @@
+#idk, let's take a string and print it backwards
+
+w = "Musicophilia"
+# rev = w[::-1]
+# print(rev)
+
+#cool. What happens if I mess with other indices
+
+print(w[::-2])
+
+#Does this work on all iterables?
+
+ls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(ls[::-3])
