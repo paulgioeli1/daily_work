28fa478ed3326b910494bc672669424e584e707a
paulgioeli1 <paulgioeli@gmail.com>
2026-01-10 22:43:05 -0800

    More edits
    Trying to get a count of duplicates working.

--- word_counter_final.py
+++ word_counter_final.py
@@ -1,19 +1,36 @@
 # Word_counter_final
+from dedupe import dedupe
 
 test_string = "You can really really count count count those duplicates count"
 test_string = test_string.lower()
 
-deduped_string = dedupe(string)
+
+word_list = []
+current_word = ""
+
+test_string = test_string + " " # I should create this as a function
+
+for a in test_string:
+    if a != " ":
+        current_word = current_word + a
+    else:
+        if current_word != "":
+            word_list.append(current_word)
+            current_word = ""
+
+
+deduped_string = dedupe(test_string)
 
 counts_list = []
 
 for i in range(len(deduped_string)):
     counter = 0
-    for j in range(len(test_string)):
-        if test_string[j] == deduped_string[i]:
+    for j in range(len(word_list)):
+        if word_list[j] == deduped_string[i]:
             counter += 1
     counts_list.append(counter)
 
+print(deduped_string)
 print(counts_list)
         
         
