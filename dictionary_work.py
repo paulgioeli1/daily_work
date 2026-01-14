90685cbb44324a0fde40d2cda7d501a7fde5749d
paulgioeli1 <paulgioeli@gmail.com>
2026-01-13 22:33:20 -0800

    Update dictionary_work.py

--- dictionary_work.py
+++ dictionary_work.py
@@ -9,3 +9,18 @@ print(type(dict_1.values()))
 
 key_list = list(dict_1.keys())
 print(key_list)
+
+value_list = list(dict_1.values())
+print(value_list)
+
+var1 = ''
+for i in range(len(key_list)):
+    var1 = var1 + key_list[i]
+
+print(var1)
+
+var2 = 0
+for i in range(len(value_list)):
+    var2 = var2 + value_list[i]
+
+print(var2)
