618dcbfb498b8e62a0712c5e7d62025c11ecba1d
paulgioeli1 <paulgioeli@gmail.com>
2026-01-13 22:30:08 -0800

    Create dictionary_work.py

---
+++ dictionary_work.py
@@ -0,0 +1,11 @@
+#let's do some dict work.
+
+dict_1 = {'a':1, 'b':2, 'c':3, 'd':4}
+
+print(dict_1['a'])
+print(dict_1.keys())
+print(type(dict_1.keys()))
+print(type(dict_1.values()))
+
+key_list = list(dict_1.keys())
+print(key_list)
